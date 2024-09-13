26d134a754da7d1ffc291a5d7fe8987d2d045d23
shredz <shredz@centrum.cz>
2024-09-13 22:05:25 +0200

    add data insertion to the database

--- src/Controller/YetiController.php
+++ src/Controller/YetiController.php
@@ -2,15 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Yeti;
+use App\Form\YetiType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class YetiController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route(path: '/', name: 'index')]
     public function index(): Response
     {
         return $this->render('app/index.html.twig');
+    }
+
+    // add a Yeti to the database
+    #[Route(path: '/add-yeti', name: 'add-yeti')]
+    public function addYeti(Request $request)
+    {
+        $yeti = new Yeti();
+
+        $form = $this->createForm(YetiType::class, $yeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($yeti);
+            $this->em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('app/add-yeti.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Entity/Yeti.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\YetiRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: YetiRepository::class)]
+class Yeti
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $height = null;
+
+    #[ORM\Column]
+    private ?int $weight = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $rating = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): static
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): static
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+}
